9a9995e24070af84b0f158b9cb31e7a09fe04421
notKshen <shenkobe.111@gmail.com>
2024-09-23 22:45:07 -0700

    Initial Commit

---
+++ PhotoTranslateApp/PhotoTranslateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoTranslateAppApp.swift
+//  PhotoTranslateApp
+//
+//  Created by Kobe Shen on 2024-09-23.
+//
+
+import SwiftUI
+
+@main
+struct PhotoTranslateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
